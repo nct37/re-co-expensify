df26631e83e659a1a024f01b359b62db5849c78e
Nick Tanner <tannernc.nt@gmail.com>
2019-01-09 23:21:56 -0500

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -49,32 +49,6 @@ module.exports = env => {
   };
 };
 
-// module.exports = {
-//   entry: './src/app.js',
-//   output: {
-//     path: path.join(__dirname, 'public'),
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         loader: 'babel-loader',
-//         test: /\.js$/,
-//         exclude: /node_modules/
-//       },
-//       {
-//         test: /\.s?css$/,
-//         use: ['style-loader', 'css-loader', 'sass-loader']
-//       }
-//     ]
-//   },
-//   devtool: 'cheap-module-eval-source-map',
-//   devServer: {
-//     contentBase: path.join(__dirname, 'public'),
-//     historyApiFallback: true
-//   }
-// };
-
 // loader - run babel through webpack
 // test - containing a certain file
 // exclude - run .js file NOT in node_modules folder
